da909da26fa2e7e623c1b8f8d192999454c90639
Avillameza <villa.k.ana@gmail.com>
2021-08-20 10:10:46 -0700

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  #declare and reroute so that the url is not pages/X
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #Setting the home page, does not have to be called homw
+  root to: 'pages#home'
 end
