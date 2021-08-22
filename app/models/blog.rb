24156490a76bff1f7e83ad9a8b22a9f834b59152
Avillameza <villa.k.ana@gmail.com>
2021-08-22 11:26:16 -0700

    Implemented validations for blogs, skills, and portfolio items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,6 @@ class Blog < ApplicationRecord
     enum status: [:draft, :published], _default: :draft
     extend FriendlyId
     friendly_id :title, use: :slugged
+
+    validates_presence_of :title, :body
 end
