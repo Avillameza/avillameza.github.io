23b4949e9b18f12311c6d0fd578c7b9501b6b604
Avillameza <villa.k.ana@gmail.com>
2021-08-22 23:49:10 -0700

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,3 +43,13 @@ end
         thumb_image: "https://via.placeholder.com/350x150"
     )
 end
+
+puts "9 portfolio items created"
+
+3.times do |technology|
+    Portfolio.last.technologies.create!(
+        name: "Technology #{technology}",
+    )
+end
+
+puts "3 technologies created"
