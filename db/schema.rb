99b9332b938a7e6eb302e6a91ec7040ba13822ff
Avillameza <villa.k.ana@gmail.com>
2021-08-28 20:30:09 -0700

    Added new parameters for user registration and editing account

--- db/schema.rb
+++ db/schema.rb
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_08_26_200830) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "router_name"
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
