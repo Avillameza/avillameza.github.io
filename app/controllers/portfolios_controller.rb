6d4b0ee23d19572cce3b93ccc70fb2a6b3bc48fa
Avillameza <villa.k.ana@gmail.com>
2021-09-09 09:58:11 -0700

    Refactored before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,65 +1,63 @@
 class PortfoliosController < ApplicationController
-    layout "portfolio"
+  before_action :set_portfolio_item, only: %i[ show edit update destroy ]
+  layout "portfolio"
 
-    def index
-        @portfolio_items = Portfolio.all
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: "Your portfolio item is now live." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def new
-        @portfolio_item = Portfolio.new
-        3.times { @portfolio_item.technologies.build }
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: "The recordwas successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def create
-        @portfolio_item = Portfolio.new(portfolio_params)
-    
-        respond_to do |format|
-          if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: "Your portfolio item is now live." }
-          else
-            format.html { render :new, status: :unprocessable_entity }
-          end
-        end
-      end
+  def show
+  end
 
-      def edit 
-        @portfolio_item = Portfolio.find(params[:id])
-      end
+  def destroy
+    #Destroy/Delete the record
+    @portfolio_item.destroy
 
-      def update
-        @portfolio_item = Portfolio.find(params[:id])
+    #Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Record was successfully destroyed." }
+    end
+  end
 
-        respond_to do |format|
-          if @portfolio_item.update(portfolio_params)
-            format.html { redirect_to portfolios_path, notice: "The recordwas successfully updated." }
-          else
-            format.html { render :edit, status: :unprocessable_entity }
-          end
-        end
-      end
+  private
+  def portfolio_params
+    params.require(:portfolio).permit(:title, 
+                                      :subtitle, 
+                                      :body, 
+                                      technologies_attributes: [:name])
+  end
 
-      def show
-        @portfolio_item = Portfolio.find(params[:id])
-      end
-
-      def destroy
-        #Perform the lookup
-        @portfolio_item = Portfolio.find(params[:id])
-
-        #Destroy/Delete the record
-        @portfolio_item.destroy
-
-        #Redirect
-        respond_to do |format|
-          format.html { redirect_to portfolios_url, notice: "Record was successfully destroyed." }
-        end
-      end
-
-      private
-      def portfolio_params
-        params.require(:portfolio).permit(:title, 
-                                          :subtitle, 
-                                          :body, 
-                                          technologies_attributes: [:name])
-      end
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
