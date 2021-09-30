b0fba6ff4212d15b0c51165cbbdc698eb3b17faf
Avillameza <villa.k.ana@gmail.com>
2021-09-29 23:58:27 -0700

    Updated migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_043755) do
+ActiveRecord::Schema.define(version: 2021_09_30_061109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_09_22_043755) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
@@ -67,17 +67,16 @@ ActiveRecord::Schema.define(version: 2021_09_22_043755) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "router_name"
-    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "roles"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "blogs"
   add_foreign_key "technologies", "portfolios"
 end
