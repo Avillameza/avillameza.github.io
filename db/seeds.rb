df20f6d99cc5bfee5ad2071c8fd7595c2849db56
Avillameza <villa.k.ana@gmail.com>
2021-09-30 00:05:43 -0700

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-10.times do |blog|
+1.times do |blog|
     Blog.create!(
-        title: "My Blog Post #{blog}",
-        body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium"
+        title: "Non-Software Projects| Blog #{blog}",
+        body: "You can see my non-sofware portfolio at https://villaanaengineering.files.wordpress.com/2018/05/ana-villa_engineering-portfolio.pdf"
     )
 end
 
-puts "10 blog posts created"
+puts "1 blog post created"
 
 5.times do |skill|
     Skill.create!(
@@ -16,20 +16,10 @@ end
 
 puts "5 skills created"
 
-8.times do |portfolio_item|
-    Portfolio.create!(
-        title: "Portfolio title: #{portfolio_item}",
-        subtitle: "Ruby on Rails",
-        body: "totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur",
-        main_image: "https://via.placeholder.com/600x400",
-        thumb_image: "https://via.placeholder.com/350x150"
-    )
-end
-
 1.times do |portfolio_item|
     Portfolio.create!(
         title: "Portfolio title: #{portfolio_item}",
-        subtitle: "Angular",
+        subtitle: "Ruby on Rails",
         body: "totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur",
         main_image: "https://via.placeholder.com/600x400",
         thumb_image: "https://via.placeholder.com/350x150"
